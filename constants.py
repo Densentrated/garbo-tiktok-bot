98b67f84e69b94b9db94d16a52e4eb92bcbcea39
D-108 <therealdensentrated+git-commits@gmail.com>
2024-01-23 20:59:07 -0800

    Created Some More Files

---
+++ constants.py
@@ -0,0 +1,3 @@
+# Reddit URL's
+# 4chan URL's
+# Twitter URL's
---
+++ main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# scrapes data from reddit
+# Picks a random Video Background from a list of videos and time sections
+# Turns the reddit post into an audio file
