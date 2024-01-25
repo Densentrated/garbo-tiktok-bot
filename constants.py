2d280e9732650b434792179bbe39eb1f47dbaa69
D-108 <72484574+Densentrated@users.noreply.github.com>
2024-01-24 21:18:54 -0800

    Tried to Merge

--- constants.py
+++ constants.py
@@ -1,1 +1,12 @@
+<<<<<<< Updated upstream
 # Reddit URL's
+=======
+#!/usr/bin/env python3
+"""Constants for the Program Go Here"""
+# URL Constants
+
+
+# Video Creation Constants
+NUM_SONGS = 0
+NUM_VIDEOS = 0
+>>>>>>> Stashed changes
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
+<<<<<<< Updated upstream
 
 # scrapes data from reddit
 # Picks a random Video Background from a list of videos and time sections
 # Turns the reddit post into an audio file
+=======
+"""Code Entry Point"""
+# thrid party
+import os
+import random
+
+#local
+import constants
+
+# Creates Directories If Needed
+# Scrape the Web
+# Pick a video
+# Pick a background track
+# 
+>>>>>>> Stashed changes
