8ecdb7491ba0c99c6bbf5b180def9036ab54c3f9
D-108 <therealdensentrated+git-commits@gmail.com>
2024-01-28 19:53:20 -0800

    Added Post to Speech Functionality
    - Added a test post
    - added post to speech functionality

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 """Code Entry Point"""
 # thrid party
 import os
-
-# import random
+import gtts
+import pydub
 
 # local
 import constants
 
-# Does all the prerequisite work needed
+# Creates Directories
 empty_directories = False
 
 for directory in constants.FULL_DIRECTORY_LIST:
@@ -19,10 +19,9 @@ for directory in constants.FULL_DIRECTORY_LIST:
         print("Creating Directory: " + directory)
     else:
         print(directory + " Found")
-
 print()
-# Checks if directories are empty
 
+# Checks if directories are empty
 for directory in constants.SEED_DIRECTORY_LIST:
     if not os.listdir(directory):
         print(directory + " is empty")
@@ -32,9 +31,28 @@ if empty_directories:
     print("Consult the README.md to fill directories")
     exit()
 
-# Picks a video and background track at random
-# Scrape the Web and find a random top post of the day
-# Generate Audio for the Post
-# Generate Captions for the Post
 
-# Pick a background track
+# turns post into audio
+reddit_post = open("post.txt", "r").read()
+post_audio = gtts.gTTS(reddit_post)
+# saves the audio of the post to a file
+post_audio.save(constants.GENERATED_AUDIO_DIRECTORY + "/post.mp3")
+# speeds the audio of the post up by a bit
+post_audio = pydub.AudioSegment.from_mp3(
+    constants.GENERATED_AUDIO_DIRECTORY + "/post.mp3"
+)
+post_audio = post_audio.speedup(playback_speed=1.3)
+# saves the sped up audio of the post to a file
+post_audio.export(constants.GENERATED_AUDIO_DIRECTORY + "/post.mp3", format="mp3")
+
+# grabs length of audio file
+
+# grabs clip the video that is as long as the audio file
+
+# merges the audio and video clip together
+
+# saves the video clip to the final directory
+
+
+print("Post Audio Generated")
+exit()
