2f72b6f225894506a82cdac92f0048ee33facc76
D-108 <72484574+Densentrated@users.noreply.github.com>
2024-01-29 22:40:06 -0800

    Added random clip functionality

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 import os
 import gtts
 import pydub
+import random
+from moviepy.editor import AudioFileClip, VideoFileClip
 
 # local
 import constants
@@ -31,7 +33,10 @@ if empty_directories:
     print("Consult the README.md to fill directories")
     exit()
 
-
+# Picks a video and background track at random
+# Scrape the Web and find a random top post of the day
+# Generate Audio for the Post
+# Generate Captions for the Post
 # turns post into audio
 reddit_post = open("post.txt", "r").read()
 post_audio = gtts.gTTS(reddit_post)
@@ -43,16 +48,35 @@ post_audio = pydub.AudioSegment.from_mp3(
 )
 post_audio = post_audio.speedup(playback_speed=1.3)
 # saves the sped up audio of the post to a file
-post_audio.export(constants.GENERATED_AUDIO_DIRECTORY + "/post.mp3", format="mp3")
-
+post_audio.export(constants.GENERATED_AUDIO_DIRECTORY +
+                  "/post.mp3", format="mp3")
 # grabs length of audio file
-
+audio_clip = pydub.AudioSegment.from_file(constants.GENERATED_AUDIO_DIRECTORY +
+                                          "/post.mp3")
+audio_duration = int(audio_clip.duration_seconds + 1)
+print(audio_duration)
 # grabs clip the video that is as long as the audio file
+seed_video_duration = VideoFileClip(constants.BACKGROUND_VIDEO_DIRECTORY +
+                                    "/Video0.mp4")
+seed_video_duration = int(seed_video_duration.duration - 1)
+print(seed_video_duration)
 
-# merges the audio and video clip together
+final_post_start = random.randint(audio_duration, seed_video_duration)
+final_post_end = final_post_start - audio_duration
+final_post = VideoFileClip(constants.BACKGROUND_VIDEO_DIRECTORY +
+                           "/Video0.mp4").subclip(final_post_end,
+                                                  final_post_start)
 
-# saves the video clip to the final directory
+# overlays the speech audio over the video
+
+final_post_audio = AudioFileClip(constants.GENERATED_AUDIO_DIRECTORY +
+                                 "/post.mp3")
+final_post = final_post.set_audio(final_post_audio)
+# overlays music over the video
+
+# renders the video
+final_post.write_videofile(constants.GENERATED_VIDEO_DIRECTORY + "/video.mp4",
+                           codec="libx264", audio_codec="mp3", bitrate="5000k")
 
 
-print("Post Audio Generated")
 exit()
